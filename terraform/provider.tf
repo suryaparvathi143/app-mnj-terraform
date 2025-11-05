8f023ade99c10738271b2c4d0398d83af74ff465
naveen kumar <navee@MacBook-Pro.local>
2025-11-05 16:34:35 -0500

    first commit

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,8 +1,3 @@
 provider "aws" {
   region = var.aws_region
-  # Credentials are intentionally not hardcoded.
-  # Terraform will read credentials from:
-  # - Environment variables: AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_SESSION_TOKEN
-  # - Or AWS CLI configured profile (AWS_PROFILE)
-  # - Or IAM role attached to the Jenkins/EC2 instance (instance profile)
 }
