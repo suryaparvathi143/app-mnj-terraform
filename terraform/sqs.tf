0e309feba287129e0a954487dee91abc2761046b
naveen kumar <navee@MacBook-Pro.local>
2025-11-05 16:16:22 -0500

    first commit

--- terraform/sqs.tf
+++ terraform/sqs.tf
@@ -29,11 +29,13 @@ data "aws_iam_policy_document" "s3_to_sqs" {
   }
 }
 
+# Attach the S3-to-SQS policy to the queue
 resource "aws_sqs_queue_policy" "allow_s3" {
   queue_url = aws_sqs_queue.app_queue.id
   policy    = data.aws_iam_policy_document.s3_to_sqs.json
 }
-# Configure S3 bucket notification to send events to SQS.
+
+# Configure S3 bucket notification to send events to SQS
 resource "aws_s3_bucket_notification" "bucket_notification" {
   bucket = aws_s3_bucket.app_bucket.id
 
@@ -41,15 +43,12 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
     queue_arn = aws_sqs_queue.app_queue.arn
     events    = var.s3_events
 
-    # Optional filter, applied only if suffix is provided
-    dynamic "filter" {
-      for_each = var.s3_filter_suffix != "" ? [1] : []
-      content {
-        key {
-          filter_rules {
-            name  = "suffix"
-            value = var.s3_filter_suffix
-          }
+    # Optional filter (you can keep or remove this block)
+    filter {
+      key {
+        filter_rules {
+          name  = "suffix"
+          value = var.s3_filter_suffix
         }
       }
     }
