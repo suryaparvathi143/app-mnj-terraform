d941bc27f082c35b28676f8196a83f4e05a2a30a
naveen kumar <navee@MacBook-Pro.local>
2025-11-06 16:15:10 -0500

    first commit

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,15 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-}
-
 provider "aws" {
-  region                   = var.aws_region
-  access_key               = var.aws_access_key
-  secret_key               = var.aws_secret_key
-  shared_credentials_files = []  # disables use of local ~/.aws/credentials
+  region= var.aws_region
+   profile = "terraformuser"
 }
