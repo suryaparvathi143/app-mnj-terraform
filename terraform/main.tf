4c292d3c1cba8a7406345cf2e0010849457645ba
naveen kumar <navee@MacBook-Pro.local>
2025-11-06 16:02:05 -0500

    first commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,6 @@ resource "aws_s3_bucket" "springboot_bucket" {
   bucket = var.bucket_name
 
   tags = {
-    Name        = "springboot-terraform-bucket"
+    Name = "springboot-terraform-bucket"
   }
 }
