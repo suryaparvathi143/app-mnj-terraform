a5d7e2398d2ab01d77027144701530c8ca71b4cf
naveen kumar <navee@MacBook-Pro.local>
2025-11-06 14:00:20 -0500

    first commit

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,22 @@
-variable "aws_access_key" {
-  description = "AWS Access Key"
+variable "bucket_name" {
+  description = "Name of the S3 bucket to be created"
   type        = string
 }
 
-variable "aws_secret_key" {
-  description = "AWS Secret Key"
+variable "aws_region" {
+  description = "AWS region where resources will be created"
   type        = string
+  default     = "us-east-1"
+}
+
+variable "aws_access_key" {
+  description = "AWS Access Key ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Access Key"
+  type        = string
+  sensitive   = true
 }
