2899f3399dba7a0f2dc90280ef6eb98c3c2e8a61
naveen kumar <navee@MacBook-Pro.local>
2025-11-06 13:52:21 -0500

    first commit

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,12 +4,6 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-variable "aws_profile" {
-  description = "AWS CLI profile name"
-  type        = string
-  default     = "default"
-}
-
 variable "bucket_name" {
   description = "S3 bucket name"
   type        = string
