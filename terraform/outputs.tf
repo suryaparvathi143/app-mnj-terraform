2e7d48e30c2809395c7dcb4f969628bfda66220d
naveen kumar <navee@MacBook-Pro.local>
2025-11-07 14:51:02 -0500

    first commit

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,10 +1,9 @@
 output "s3_bucket_name" {
-  value       = var.bucket_name
-  description = "S3 bucket name"
-  condition   = var.create_s3
+  description = "S3 bucket name (if created)"
+  value       = var.create_s3 ? var.bucket_name : "S3 bucket creation skipped"
 }
 
 output "sqs_queue_url" {
-  value       = aws_sqs_queue.app_queue.id
   description = "SQS Queue URL"
+  value       = aws_sqs_queue.app_queue.id
 }
